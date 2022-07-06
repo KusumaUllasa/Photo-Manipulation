ee033047d607a594b61eb4eac3d83703256d5b73
KusumaUllasa <108713931+KusumaUllasa@users.noreply.github.com>
2022-07-06 14:27:57 +0530

    Create Manipulating.py

---
+++ PIL_Pillow/Manipulating.py
@@ -0,0 +1,47 @@
+# Using .transpose(), .rotate() to flip and rotate images.
+
+>>> converted_img = img.transpose(Image.FLIP_TOP_BOTTOM)
+>>> converted_img.show()
+
+>>> rotated_img = img.rotate(45)
+>>> rotated_img.show()
+>>> rotated_img = img.rotate(45, expand=True)
+>>> rotated_img.show()
+
+# Blurring, sharpening and smoothing
+
+>>> from PIL import Image, ImageFilter
+>>> filename = "buildings.jpg"
+>>> with Image.open(filename) as img:
+...     img.load()
+>>> blur_img = img.filter(ImageFilter.BLUR)
+>>> blur_img.show()
+>>> img.crop((300, 300, 500, 500)).show()
+>>> blur_img.crop((300, 300, 500, 500)).show()
+
+>>> img.filter(ImageFilter.BoxBlur(5)).show()
+>>> img.filter(ImageFilter.BoxBlur(20)).show()
+>>> img.filter(ImageFilter.GaussianBlur(20)).show()
+
+>>> sharp_img = img.filter(ImageFilter.SHARPEN)
+>>> img.crop((300, 300, 500, 500)).show()
+>>> sharp_img.crop((300, 300, 500, 500)).show()
+
+>>> smooth_img = img.filter(ImageFilter.SMOOTH)
+>>> img.crop((300, 300, 500, 500)).show()
+>>> smooth_img.crop((300, 300, 500, 500)).show()
+
+# Edge Detection, Edge Enhancement, and Embossing
+
+>>> img_gray = img.convert("L")
+>>> edges = img_gray.filter(ImageFilter.FIND_EDGES)
+>>> edges.show()
+
+>>> img_gray_smooth = img_gray.filter(ImageFilter.SMOOTH)
+>>> edges_smooth = img_gray_smooth.filter(ImageFilter.FIND_EDGES)
+>>> edges_smooth.show()
+
+>>> edge_enhance = img_gray_smooth.filter(ImageFilter.EDGE_ENHANCE)
+>>> edge_enhance.show()
+>>> emboss = img_gray_smooth.filter(ImageFilter.EMBOSS)
+>>> emboss.show()
