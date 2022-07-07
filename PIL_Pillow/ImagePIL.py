e159ea04d1d179107aaf21a40a10276e75aa58e5
KusumaUllasa <108713931+KusumaUllasa@users.noreply.github.com>
2022-07-07 10:20:03 +0530

    Update ImagePIL.py

--- PIL_Pillow/ImagePIL.py
+++ PIL_Pillow/ImagePIL.py
@@ -1,42 +1,36 @@
-PS> python -m venv venv
-PS> .\venv\Scripts\activate
-(venv) PS> python -m pip install Pillow
-
 #Image reading and storing a temporary file
 
->>> from PIL import Image
->>> filename = "buildings.jpg"
->>> with Image.open(filename) as img:
-...     img.load()
-...
+from PIL import Image
+filename = "buildings.jpg"
+with Image.open(filename) as img:
+img.load()
 
->>> type(img)
+type(img)
 <class 'PIL.JpegImagePlugin.JpegImageFile'>
 
->>> isinstance(img, Image.Image)
+isinstance(img, Image.Image)
 True
->>> img.show()
+img.show()
 
->>> img.format
+img.format
 'JPEG'
 
->>> img.size
+img.size
 (1920, 1273)
 
->>> img.mode
+img.mode
 'RGB'
->>> cropped_img = img.crop((300, 150, 700, 1000))
->>> cropped_img.size
+cropped_img = img.crop((300, 150, 700, 1000))
+cropped_img.size
 (400, 850)
 
->>> cropped_img.show()
+cropped_img.show()
 
->>> low_res_img = cropped_img.resize(
-...     (cropped_img.width // 4, cropped_img.height // 4)
-... )
->>> low_res_img.show()
+low_res_img = cropped_img.resize(
+(cropped_img.width // 4, cropped_img.height // 4)
+)
+low_res_img.show()
+low_res_img = cropped_img.reduce(4)
 
->>> low_res_img = cropped_img.reduce(4)
-
->>> cropped_img.save("cropped_image.jpg")
->>> low_res_img.save("low_resolution_cropped_image.png")
+cropped_img.save("cropped_image.jpg")
+low_res_img.save("low_resolution_cropped_image.png")
